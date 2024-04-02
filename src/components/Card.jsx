c96d57bede57e3cc44e914616585e63eb3cb4f7e
pratham <darkvoid6565@gmail.com>
2024-04-02 12:32:16 +0530

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const Card = ({name,referenceNumber,totalItems,totalWeight,status}) => {
+  return (
+    <div className='flex flex-col gap-7  border-red-600 border w-[22%] rounded-md px-4 py-5 relative' >
+        {/* item name and reference number */}
+        <div className='flex flex-col gap-2 justify-center items-center'>
+            <h2 className='font-extrabold'>
+                {name}
+            </h2>
+            <div>
+                Ref: <span className='font-extrabold'>{referenceNumber}</span>
+            </div>
+        </div>
+        {/* item name sectin ends */}
+        {/* dimension and status of items */}
+        <div className='flex flex-col justify-center text-xs pb-4'>
+            <div>
+                Total items: <span className='font-extrabold text-xl'>{totalItems}</span>
+            </div>
+            <div>
+                Total weight: <span className='font-extrabold  text-xl'>{totalWeight}</span>
+            </div>
+            <div>
+                Status: <span className='font-extrabold text-red-600 text-xl'>{status}</span>
+            </div>
+        </div>
+        {/* dimension section ends */}
+        {/* update dimension button */}
+        <div className='absolute bottom-9 right-[-10px] rounded-lg  bg-red-500 p-2'>
+            <button className='w-24 text-white font-semibold'>
+                Update Dimension
+            </button>
+        </div>
+    </div>
+  )
+}
+
+Card.propTypes = {
+    name: PropTypes.string.isRequired,
+    referenceNumber: PropTypes.string.isRequired,
+    totalItems: PropTypes.number.isRequired,
+    totalWeight: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+  };
+  
+export default Card
---
+++ src/components/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Card from './Card'
+const cardsData = [
+    { name: 'Crockerys House', referenceNumber: 'IBC', totalWeight: '85', totalItems: 7, status: 'Pending' },
+    { name: 'Crockerys House', referenceNumber: 'IBC', totalWeight: '85', totalItems: 7, status: 'Pending' },
+    { name: 'Crockerys House', referenceNumber: 'IBC', totalWeight: '85', totalItems: 7, status: 'Pending' },
+    { name: 'Crockerys House', referenceNumber: 'IBC', totalWeight: '85', totalItems: 7, status: 'Pending' },
+    { name: 'Crockerys House', referenceNumber: 'IBC', totalWeight: '85', totalItems: 7, status: 'Pending' },
+    // Add more data as needed
+  ];
+
+const Home = () => {
+  return (
+    <div className='flex flex-wrap gap-10 bg-white text-black p-24'>
+        {/* rendering all the cards */}
+        {cardsData.map((card, index) => (
+        <Card
+          key={index} // Always provide a unique key when rendering a list of components
+          name={card.name} // Pass name as a prop
+          referenceNumber={card.referenceNumber} // Pass referenceNumber as a prop
+          totalWeight={card.totalWeight} // Pass totalWeight as a prop
+          totalItems={card.totalItems} // Pass totalItems as a prop
+          status={card.status} // Pass status as a prop
+        />
+      ))}
+    </div>
+  )
+}
+
+export default Home
