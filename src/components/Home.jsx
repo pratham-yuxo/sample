72ab4d5fcc521a299ef427fca954fb4c0cd2c126
pratham <darkvoid6565@gmail.com>
2024-04-02 18:42:42 +0530

    new changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,18 +11,21 @@ const cardsData = [
 
 const Home = () => {
   return (
-    <div className='flex flex-wrap gap-10 bg-white text-black p-24'>
+    <div className='flex flex-col justify-center items-center'>
+      <div className='flex flex-wrap gap-6 lg:gap-10 bg-white text-black p-4 lg:p-24'>
         {/* rendering all the cards */}
-        {cardsData.map((card, index) => (
-        <Card
-          key={index} // Always provide a unique key when rendering a list of components
-          name={card.name} // Pass name as a prop
-          referenceNumber={card.referenceNumber} // Pass referenceNumber as a prop
-          totalWeight={card.totalWeight} // Pass totalWeight as a prop
-          totalItems={card.totalItems} // Pass totalItems as a prop
-          status={card.status} // Pass status as a prop
-        />
-      ))}
+
+          {cardsData.map((card, index) => (
+            <Card
+            key={index} // Always provide a unique key when rendering a list of components
+            name={card.name} // Pass name as a prop
+            referenceNumber={card.referenceNumber} // Pass referenceNumber as a prop
+            totalWeight={card.totalWeight} // Pass totalWeight as a prop
+            totalItems={card.totalItems} // Pass totalItems as a prop
+            status={card.status} // Pass status as a prop
+            />
+            ))}
+      </div>
     </div>
   )
 }
