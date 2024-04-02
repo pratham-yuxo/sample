1a4b1eff117b723b2252dde19524645ad92ee93d
pratham <darkvoid6565@gmail.com>
2024-04-02 18:56:16 +0530

    more changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Card from './Card'
 const cardsData = [
-    { name: 'Crockerys House', referenceNumber: 'IBC', totalWeight: '85', totalItems: 7, status: 'Pending' },
-    { name: 'Crockerys House', referenceNumber: 'IBC', totalWeight: '85', totalItems: 7, status: 'Pending' },
-    { name: 'Crockerys House', referenceNumber: 'IBC', totalWeight: '85', totalItems: 7, status: 'Pending' },
-    { name: 'Crockerys House', referenceNumber: 'IBC', totalWeight: '85', totalItems: 7, status: 'Pending' },
-    { name: 'Crockerys House', referenceNumber: 'IBC', totalWeight: '85', totalItems: 7, status: 'Pending' },
+    { name: 'Crockery House', referenceNumber: "IBC's", totalWeight: '85', totalItems: 7, status: 'Pending' },
+    { name: 'Crockery House', referenceNumber: "IBC's", totalWeight: '85', totalItems: 7, status: 'Pending' },
+    { name: 'Crockery House', referenceNumber: "IBC's", totalWeight: '85', totalItems: 7, status: 'Pending' },
+    { name: 'Crockery House', referenceNumber: "IBC's", totalWeight: '85', totalItems: 7, status: 'Pending' },
+    { name: 'Crockery House', referenceNumber: "IBC's", totalWeight: '85', totalItems: 7, status: 'Pending' },
     // Add more data as needed
   ];
 
 const Home = () => {
   return (
     <div className='flex flex-col justify-center items-center'>
-      <div className='flex flex-wrap gap-6 lg:gap-10 bg-white text-black p-4 lg:p-6'>
+      <div className='flex flex-wrap gap-6 lg:gap-8 bg-white text-black p-4 lg:p-6'>
         {/* rendering all the cards */}
 
           {cardsData.map((card, index) => (
